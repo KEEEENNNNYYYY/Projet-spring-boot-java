918dbb03f7a00161521a6f8bdf53f4b0b60d246a
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-11 10:40:25 +0200

    feat:added new controller

--- TOdoListe/src/main/java/hei/school/kenny/todoliste/Controller/TaskController.java
+++ TOdoListe/src/main/java/hei/school/kenny/todoliste/Controller/TaskController.java
@@ -2,8 +2,11 @@ package hei.school.kenny.todoliste.Controller;
 
 import hei.school.kenny.todoliste.service.TaskService;
 import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,14 +19,27 @@ public class TaskController {
     }
 
     @GetMapping("/tasks")
-    public String getAllTasks() {
+    public ResponseEntity<String> getAllTasks() {
         JSONArray tasksArray = taskService.getAllTasks();
-        return tasksArray.toString(2);
+        return new ResponseEntity<>(tasksArray.toString(2), HttpStatus.OK);
     }
 
     @GetMapping("/task/{idToFind}")
-    public String getTaskByID(@PathVariable int idToFind) {
+    public ResponseEntity<String> getTaskByID(@PathVariable int idToFind) {
         JSONArray tasksArray = taskService.getTaskByID(idToFind);
-        return tasksArray.toString(2);
+        return new ResponseEntity<>(tasksArray.toString(2), HttpStatus.OK);
+    }
+
+    @PostMapping("/task/add/{idToAdd}/{nameToAdd}/{stateToAdd}")
+    public String addTask(@PathVariable int idToAdd,
+                          @PathVariable String nameToAdd,
+                          @PathVariable String stateToAdd) {
+        return taskService.addTask(idToAdd, nameToAdd, stateToAdd);
+    }
+
+    @GetMapping("/task/search/{stateToSearch}")
+    public ResponseEntity<String> searchTaskByState(@PathVariable String stateToSearch) {
+        JSONArray tasksArray = taskService.searchTaskByState(stateToSearch);
+        return new ResponseEntity<>(tasksArray.toString(), HttpStatus.OK);
     }
 }
