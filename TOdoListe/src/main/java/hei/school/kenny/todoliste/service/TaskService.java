7d28dcffdb971dcdc2dff960cc81f735e817fc20
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-11 10:45:18 +0200

    update:new controller search by id

--- TOdoListe/src/main/java/hei/school/kenny/todoliste/service/TaskService.java
+++ TOdoListe/src/main/java/hei/school/kenny/todoliste/service/TaskService.java
@@ -1,5 +1,7 @@
 package hei.school.kenny.todoliste.service;
 
+import hei.school.kenny.todoliste.DAO.AddTaskDAO;
+import hei.school.kenny.todoliste.DAO.SearchTaskByStateDAO;
 import hei.school.kenny.todoliste.DAO.TaskDAO;
 import hei.school.kenny.todoliste.DAO.TaskByID_DAO;
 import org.json.JSONArray;
@@ -9,10 +11,19 @@ import org.springframework.stereotype.Service;
 public class TaskService {
     private final TaskDAO taskDAO;
     private final TaskByID_DAO taskByID_DAO;
+    private final AddTaskDAO addTaskDAO;
+    private final SearchTaskByStateDAO searchTaskByStateDAO;
 
-    public TaskService(TaskDAO taskDAO, TaskByID_DAO taskByID_DAO) {
+    public TaskService(
+            TaskDAO taskDAO,
+            TaskByID_DAO taskByID_DAO,
+            AddTaskDAO addTaskDAO,
+            SearchTaskByStateDAO searchTaskByStateDAO
+    ) {
         this.taskDAO = taskDAO;
         this.taskByID_DAO = taskByID_DAO;
+        this.addTaskDAO = addTaskDAO;
+        this.searchTaskByStateDAO = searchTaskByStateDAO;
     }
 
     public JSONArray getAllTasks() {
@@ -22,4 +33,14 @@ public class TaskService {
     public JSONArray getTaskByID(int idToFind) {
         return taskByID_DAO.fetchTaskByID(idToFind);
     }
+
+    public String addTask(int idToAdd, String nameToAdd, String stateToAdd) {
+        return addTaskDAO.addTask(idToAdd, nameToAdd, stateToAdd);
+    }
+
+    public JSONArray searchTaskByState(String stateToSearch) {
+        return searchTaskByStateDAO.fetchTaskByState(stateToSearch);
+    }
 }
+
+
